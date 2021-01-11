1fa7893202db7cd0b45e25140707f1448fd50471
chang sik kwon <kcs1987@gamil.com>
2021-01-12 01:58:54 +0900

    Modify: schema 위치변경

--- schema.py
+++ schema.py
@@ -9,28 +9,12 @@ import graphene
 class User(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
-        interfaces = (relay.Node, )
-        
-
-class UserCon(relay.Connection):
-    class Meta:
-        node = User
 
         
 class ToDoList(SQLAlchemyObjectType):
     class Meta:
         model = ToDoListModel
-        interface = (relay.Node, )
-        
-
-class ToDoListCon(relay.Connection):
-    class Meta:
-        node = ToDoList
 
 
 class Query(graphene.ObjectType):
     node = relay.Node.Field()
-    all_users = SQLAlchemyConnectionField(UserCon)
-    all_to_do_lists = SQLAlchemyConnectionField(ToDoListCon)
-    
-schema = graphene.Schema(query = Query, mutation = Mutations)
