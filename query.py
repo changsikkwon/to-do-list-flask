c4bb0e350c16fc578d6682f2ccc41a97bee52a97
chang sik kwon <kcs1987@gamil.com>
2021-01-12 17:25:07 +0900

    Add: user list to do list query 추가

--- query.py
+++ query.py
@@ -2,7 +2,7 @@ from graphene_sqlalchemy import SQLAlchemyObjectType
 from flask import g
 
 from models import User as UserModel, ToDoList as ToDoListModel
-from utils import login_required
+from utils import login_required, master_required
 
 import graphene
 
@@ -17,4 +17,26 @@ class ToDoList(SQLAlchemyObjectType):
 
 
 class Query(graphene.ObjectType):
-    to_do_list = graphene.List(ToDoList)
+    to_do_list_by_user = graphene.List(ToDoList)
+    to_do_list_by_master = graphene.List(ToDoList, user_id=graphene.Int(required=True))
+    user_list_by_master = graphene.List(User, offset=graphene.Int(required=True))
+    
+    @login_required
+    def resolve_to_do_list_by_user(self, info):
+        user_id = g.user_id['user_id']
+        to_do_query = ToDoList.get_query(info)
+        
+        return to_do_query.filter(ToDoListModel.user_id.contains(user_id))
+    
+    @master_required
+    def resolve_to_do_list_by_master(self, info, user_id):
+        to_do_query = ToDoList.get_query(info)
+        
+        return to_do_query.filter(ToDoListModel.user_id.contains(user_id))
+    
+    @master_required
+    def resolve_user_list_by_master(self, info, offset):
+        user_query = User.get_query(info)
+        
+        return user_query.filter(UserModel.is_master==0).limit(10).offset(offset)
+        
